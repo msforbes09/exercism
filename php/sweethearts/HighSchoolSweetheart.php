6e50d4db716175f95fea43f7a0eddddcfd172bfd
Arnel Forbes <forbesarnel09@gmail.com>
2025-08-18 15:17:22 +0800

    :white_check_mark: Solution 3: sweetheart

--- php/sweethearts/HighSchoolSweetheart.php
+++ php/sweethearts/HighSchoolSweetheart.php
@@ -4,21 +4,43 @@ class HighSchoolSweetheart
 {
     public function firstLetter(string $name): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        return substr(trim($name), 0, 1);
     }
 
     public function initial(string $name): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        return strtoupper($this->firstLetter($name)) . '.';
     }
 
     public function initials(string $name): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        $initials = '';
+
+        [$firstName, $lastName] = explode(' ', trim($name));
+
+        return $this->initial($firstName) . ' ' . $this->initial($lastName);
     }
 
     public function pair(string $sweetheart_a, string $sweetheart_b): string
     {
-        throw new \BadFunctionCallException("Implement the function");
+        $initials = $this->initials($sweetheart_a) . '  +  ' . $this->initials($sweetheart_b);
+
+        $heart = '';
+        $heart .= "     ******       ******\n";
+        $heart .= "   **      **   **      **\n";
+        $heart .= " **         ** **         **\n";
+        $heart .= "**            *            **\n";
+        $heart .= "**                         **\n";
+        $heart .= "**     $initials     **\n";
+        $heart .= " **                       **\n";
+        $heart .= "   **                   **\n";
+        $heart .= "     **               **\n";
+        $heart .= "       **           **\n";
+        $heart .= "         **       **\n";
+        $heart .= "           **   **\n";
+        $heart .= "             ***\n";
+        $heart .= '              *';
+
+        return $heart;
     }
 }
