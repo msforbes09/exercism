f9093273470441f44d3bac9d85978d5f850c9688
Arnel Forbes <forbesarnel09@gmail.com>
2025-08-19 10:18:00 +0800

    :white_check_mark: Solution: `Language List`

--- php/language-list/LanguageList.php
+++ php/language-list/LanguageList.php
@@ -1,6 +1,30 @@
 <?php
 
-function language_list()
+function language_list(...$list)
 {
-    // implement the language list function
+    return $list;
+}
+
+function add_to_language_list(array $list, $language)
+{
+    $list[] = $language;
+
+    return $list;
+}
+
+function prune_language_list(array $list)
+{
+    array_shift($list);
+
+    return $list;
+}
+
+function current_language(array $list)
+{
+    return $list[0];
+}
+
+function language_list_length(array $list)
+{
+    return count($list);
 }
